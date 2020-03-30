de9aadb698d622d20411bb2bc2b6690bbfe07218
codingmr <codingmroberts@gmail.com>
2020-03-30 19:07:50 +0100

    deploy

---
+++ precache-manifest.242f04d40f90b67fd607018eceaf2cb8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ae72673b88597f313fa1e2b07a34d3e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9c75b45261d6f395e36c",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "b48eea20ba83970d680a",
+    "url": "/static/css/main.c14c8fbc.chunk.css"
+  },
+  {
+    "revision": "9c75b45261d6f395e36c",
+    "url": "/static/js/2.7c0cebef.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/static/js/2.7c0cebef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b48eea20ba83970d680a",
+    "url": "/static/js/main.0142643a.chunk.js"
+  },
+  {
+    "revision": "dba6a9c0e2f32d7d6855",
+    "url": "/static/js/runtime-main.7abb02b6.js"
+  },
+  {
+    "revision": "4e14772198c0adb2dd390ebe87747cf0",
+    "url": "/static/media/net_graph.4e147721.png"
+  }
+]);
