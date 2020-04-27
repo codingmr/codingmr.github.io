bec74ac9db9e7fd51bedb1e258188d51dd1141de
codingmr <codingmroberts@gmail.com>
2020-04-27 23:56:17 +0100

    overline v1

--- src/components/OverlineItem.js
+++ src/components/OverlineItem.js
@@ -2,32 +2,31 @@ import React from 'react';
 
 import './styles/overlineItem-default.css';
 
-import gimp from '../media/ic_gimp.svg';
-
 
 export default class OverlineItem extends React.Component{
-  constructor(props) {
-    super(props);
+  constructor(props){
+    super(props)
 
     this.state = {
-      title: props.title,
-      description: props.description,
-      iconsDesired: props.icons
-    };
+      isOpen: false
+    }
   }
 
+
+
   render() {
+    const listIcons = this.props.icons.map((icon)=><li key={icon} id="icon"><img id="icon_img" src={icon} alt="alt Icon" /></li>);
+
     return(
       <div id="container">
         <div id="body">
-          <p id="overline">{this.state.title}</p>
-          <p id="description">{this.state.description}</p>
+          <p id="overline">{this.props.title}</p>
+          <p id="description">{this.props.description}</p>
+          <div id="expand"><a>expand</a></div>
         </div>
         <div id="icon-box">
           <div id="icon-back">
-            <div id="icon"><img src={gimp} alt="GIMP Icon" /></div>
-            <div id="icon"></div>
-            <div id="icon"></div>
+            {listIcons}
           </div>
         </div>
         <hr id="divider"/>
--- src/pages/Skills.js
+++ src/pages/Skills.js
@@ -5,6 +5,35 @@ import net_graph from "../media/net_graph.png";
 import "./styles/about-default.css";
 import OverlineItem from "../components/OverlineItem.js";
 
+import gimp from '../media/icons/ic_gimp.svg';
+import adobeaftereffects from '../media/icons/ic_adobeaftereffects.svg';
+import android from '../media/icons/ic_android.svg';
+import babel from '../media/icons/ic_babel.svg';
+import bootstrap from '../media/icons/ic_bootstrap.svg';
+import css from '../media/icons/ic_css.svg';
+import git from '../media/icons/ic_git.svg';
+import google from '../media/icons/ic_google.svg';
+import github from '../media/icons/ic_github.svg';
+import inkscape from '../media/icons/ic_inkscape.svg';
+import jenkins from '../media/icons/ic_jenkins.svg';
+import js from '../media/icons/ic_js.svg';
+import lunacy from '../media/icons/ic_lunacy.svg';
+import nodejs from '../media/icons/ic_nodejs.svg';
+import npm from '../media/icons/ic_npm.svg';
+import playstore from '../media/icons/ic_playstore.svg';
+import raspberrypi from '../media/icons/ic_raspberrypi.svg';
+import react from '../media/icons/ic_react.svg';
+import reactnative from '../media/icons/ic_reactnative.svg';
+import html from '../media/icons/ic_html.svg';
+import python from '../media/icons/ic_python.svg';
+import stackoverflow from '../media/icons/ic_stackoverflow.svg';
+import java from '../media/icons/ic_java.svg';
+
+// icons still to add:
+// bash, cmd, ssh, jest, eslint, prettier, metrobundler, gradle, agile method
+// machine learning, web scraping, debian, windows, slack, zoom, edge, firefox
+// safari, opera, chrome, vivaldi, iOS, macOS, Linux
+
 import { Element } from "react-scroll";
 
 export default class Skills extends React.Component {
@@ -18,7 +47,35 @@ export default class Skills extends React.Component {
               description={
                 "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur sodales ligula in libero. Sed dignissim lacinia nunc. Curabitur tortor. Pellentesque nibh. Aenean quam. In scelerisque sem at dolor. Maecena."
               }
-              icons={["gimp", "lunacy", "inkscape", "adobe-after-effects"]}
+              icons={[gimp, lunacy, inkscape, adobeaftereffects]}
+            />
+            <OverlineItem
+              title={"Responsive deb development"}
+              description={
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur sodales ligula in libero. Sed dignissim lacinia nunc. Curabitur tortor. Pellentesque nibh. Aenean quam. In scelerisque sem at dolor. Maecena."
+              }
+              icons={[react, html, css, js, bootstrap, git, npm, nodejs]}
+            />
+            <OverlineItem
+              title={"Cross-platform mobile development"}
+              description={
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur sodales ligula in libero. Sed dignissim lacinia nunc. Curabitur tortor. Pellentesque nibh. Aenean quam. In scelerisque sem at dolor. Maecena."
+              }
+              icons={[reactnative, js, babel, git, npm, nodejs, android]}
+            />
+            <OverlineItem
+              title={"Continuos Integration, testing & deployment"}
+              description={
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur sodales ligula in libero. Sed dignissim lacinia nunc. Curabitur tortor. Pellentesque nibh. Aenean quam. In scelerisque sem at dolor. Maecena."
+              }
+              icons={[jenkins, git, github, raspberrypi, playstore]}
+            />
+            <OverlineItem
+              title={"Miscellaneous"}
+              description={
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur sodales ligula in libero. Sed dignissim lacinia nunc. Curabitur tortor. Pellentesque nibh. Aenean quam. In scelerisque sem at dolor. Maecena."
+              }
+              icons={[google, stackoverflow, java, python]}
             />
           </div>
         </div>
