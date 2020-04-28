46aabbe3e1959762762372513bf2f76c5d0801ca
codingmr <codingmroberts@gmail.com>
2020-04-28 14:17:56 +0100

    improve look and feel

--- src/components/OverlineItem.js
+++ src/components/OverlineItem.js
@@ -8,21 +8,30 @@ export default class OverlineItem extends React.Component{
     super(props)
 
     this.state = {
+      isHover: false,
       isOpen: false
     }
   }
-
-
 
   render() {
     const listIcons = this.props.icons.map((icon)=><li key={icon} id="icon"><img id="icon_img" src={icon} alt="alt Icon" /></li>);
 
     return(
       <div id="container">
-        <div id="body">
+        <div id="body" onClick={()=>this.setState({isOpen: !this.state.isOpen})}>
           <p id="overline">{this.props.title}</p>
-          <p id="description">{this.props.description}</p>
-          <div id="expand"><a>expand</a></div>
+
+          { this.state.isOpen ? (
+            <p id="description" className="description_open">
+              {this.props.description}
+            </p>
+          ):(
+            <p id="description" className="description_closed">
+              {this.props.description}
+            </p>
+            )
+          }
+
         </div>
         <div id="icon-box">
           <div id="icon-back">
