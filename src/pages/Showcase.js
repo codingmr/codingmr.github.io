0043fa93d78c50e49973387a25d375008ccff233
codingmr <codingmroberts@gmail.com>
2020-05-04 09:55:09 +0100

    add zoom animation to display phone and text

--- src/pages/Showcase.js
+++ src/pages/Showcase.js
@@ -16,7 +16,7 @@ import icon_left from '../media/arrowDoubleleft.svg';
 import './styles/showcase-default.css';
 
 import { Element } from 'react-scroll'
-
+import Zoom from 'react-reveal/Zoom';
 
 const NUMBER_PAGES = 2;
 
@@ -28,8 +28,7 @@ export default class Showcase extends React.Component {
     this.lastPage = this.lastPage.bind(this);
 
     this.state = {
-      pageSelectedIndex: 1
-    }
+      pageSelectedIndex: 1    }
   }
 
   nextPage() {
@@ -56,15 +55,15 @@ export default class Showcase extends React.Component {
                 <div id="showcase-section-body">
                     <div id="carousel">
                       <div id="screen-overlay">
+                        <Zoom>
                         <img id="scale" src={device_phone} />
-
+                        <div id="test-layer">
+                        </div>
                         <div id="iphone-navigation">
                           <a onClick={this.lastPage} id="pullLeft"><img id="icon_left" src={icon_left} /></a>
                           <a onClick={this.nextPage} id="pullRight"><img id="icon_right" src={icon_right} /></a>
-
                         </div>
                         <div id="dunno">
-
                           <img id="over" src={device_overlay} />
 
                           { (this.state.pageSelectedIndex===1) ? (
@@ -76,14 +75,15 @@ export default class Showcase extends React.Component {
                               <img id="screen-icon" src={worriedBill} />
                             </div>
                           )}
-
                         </div>
+
+                        </Zoom>
                       </div>
                     </div>
                     <div id="showcase-section-text">
 
                     { (this.state.pageSelectedIndex===1) ? (
-                      <>
+                      <Zoom right>
                       <h2 id="section-title">Showcase</h2>
                       <p id="text">
                         Check out the projects I've worked on.
@@ -91,7 +91,7 @@ export default class Showcase extends React.Component {
                       <p id="text">
                         Use the left and right navigation buttons on the display phone.
                       </p>
-                      </>
+                      </Zoom>
                     ) : (
                       <>
                       <h2 id="section-title">Bill! Split the Bill</h2>
