8776708163b24d9bd41dcfc921d9fe44a6edcbe6
codingmr <codingmroberts@gmail.com>
2020-05-04 09:00:58 +0100

    added react-reveal to landing

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -8,23 +8,36 @@ import './styles/landing-default.css';
 import github from '../media/icons/ic_github.svg';
 
 import { Element } from 'react-scroll'
+import Flip from 'react-reveal/Flip';
+
+import Fade from 'react-reveal/Fade';
+
 
 export default class Landing extends React.Component {
+
+  componentDidUpdate(){
+
+  }
+
     render() {
         return (
           <Element id="landing-view">
             <div id="align-center">
-              <h1 id="title">Matt Roberts</h1>
-              <h2 id="subtitle">Software Engineer</h2>
+              <Flip bottom when={this.componentDidMount}>
+                <h1 id="title">Matt Roberts</h1>
+                <h2 id="subtitle">Software Engineer</h2>
+              </Flip>
               <hr id="line-breaker" />
-              <div id="welcome-info-body">
-                <p id="welcome-info-text">Welcome to my portfolio!</p>
-                <p id="welcome-info-text-italic">Pizza, coffee, a pinch of magic and imagination can become reality</p>
-              </div>
+              <Fade bottom>
+                <div id="welcome-info-body">
+                  <p id="welcome-info-text">Welcome to my portfolio!</p>
+                  <p id="welcome-info-text-italic">Pizza, coffee, a pinch of magic and imagination can become reality</p>
+                </div>
 
               <a id="connect-button" href="https://github.com/codingmr" color="info">
                 <img id="icon_welcome_img" src={github} alt="Github Icon" />
               </a>
+              </Fade>
 
             </div>
         </Element>
